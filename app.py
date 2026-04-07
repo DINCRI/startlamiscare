a792d74acc1c2dec1f544911603abdd21355f2d1
DINCRI <67482192+DINCRI@users.noreply.github.com>
2026-04-07 23:04:19 +0300

    update iara

--- app.py
+++ app.py
@@ -44,7 +44,10 @@ database_url = os.environ.get("DATABASE_URL")
 
 if database_url:
     if database_url.startswith("postgres://"):
-        database_url = database_url.replace("postgres://", "postgresql://", 1)
+        database_url = database_url.replace("postgres://", "postgresql+psycopg://", 1)
+    elif database_url.startswith("postgresql://"):
+        database_url = database_url.replace("postgresql://", "postgresql+psycopg://", 1)
+
     app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 else:
     data_dir = os.path.join(BASE_DIR, "data")
